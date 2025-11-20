df6b242d38a2bb825dc3f1e4fa9c2d6c44f97bed
Zhuqing Wang <zhuquing@mercadona.es>
2025-11-20 11:38:52 +0100

    feat: pagination

--- driven/postgres-repository/sql/migration/rollback/1.0.0-create-schema-examples/R1.0.0__create-schema-examples.sql
+++ driven/postgres-repository/sql/migration/rollback/1.0.0-create-schema-examples/R1.0.0__create-schema-examples.sql
@@ -3,4 +3,20 @@ DROP TABLE public.O_EXAMPLES;
 
 DROP SEQUENCE public.o_esamples_id_seq;
 
+-- ==========================
+-- Eliminar tabla order_lines
+-- ==========================
+DROP TABLE IF EXISTS order_lines;
+
+-- ==========================
+-- Eliminar tabla orders
+-- ==========================
+DROP TABLE IF EXISTS orders;
+
+-- ==========================
+-- Eliminar tabla products
+-- ==========================
+DROP TABLE IF EXISTS products;
+
+
 DELETE FROM flyway_schema_history WHERE version = '1.0.0';
